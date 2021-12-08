f1c8c7dad8fe38067e409f82cd26b576f489e77e
Derek Baylor <dbaylor25@gmail.com>
2021-12-07 21:34:37 -0600

    authorization, and income form

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as RiIcons from 'react-icons/ri';
 import { IconContext } from 'react-icons';
 import { SideNavData } from './componentData/sideNavData';
+import { signOutUser } from '../api/auth';
 
 export default function SideNav() {
   const [sidebar, setsidebar] = useState(false);
@@ -33,6 +35,16 @@ export default function SideNav() {
                 </Link>
               </li>
             ))}
+            <li className="nav-item">
+              <button
+                onClick={signOutUser}
+                type="button"
+                className="btn sign-out-btn"
+              >
+                <RiIcons.RiSendPlaneLine />
+                <span className="sign-out-text">Sign Out</span>
+              </button>
+            </li>
           </ul>
         </nav>
       </IconContext.Provider>
