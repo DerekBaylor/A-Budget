0a18b0b592b78a1be378cf5bf11e161a3362fa27
Derek Baylor <dbaylor25@gmail.com>
2021-12-15 20:49:58 -0600

    commit for debug

--- src/views/IncomeView.js
+++ src/views/IncomeView.js
@@ -1,25 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Doughnut } from 'react-chartjs-2';
+import {
+  Chart, ArcElement, Legend, Tooltip, Title,
+} from 'chart.js';
 import { getIncomes } from '../api/data/incomeData';
 import IncomeCard from '../components/cards/IncomeCard';
 import IncomeForm from '../components/forms/IncomeForm';
-import IncomeChart from '../components/charts/IncomeChart';
+// import IncomeChart from '../components/charts/IncomeChart';
+
+Chart.register(ArcElement, Title, Legend, Tooltip);
 
 export default function IncomeView({ uid }) {
   const [incomeCards, setIncomeCards] = useState([]);
   const [editItem, setEditItem] = useState({});
-  // const [chartData, setChartData] = useState({});
+  const [chartLabels, setChartLabels] = useState([]);
+  // const [chartValues, setChartValues] = useState([]);
 
   useEffect(() => {
-    getIncomes(uid).then(setIncomeCards);
-    // getIncomes(uid).then(setChartData);
+    getIncomes(uid).then((incomeArray) => {
+      setIncomeCards(incomeArray);
+    });
   }, []);
 
+  useEffect(() => {
+    const [...cLabels] = incomeCards.map((card) => card.category);
+    const thing2 = cLabels.valueOf;
+    console.warn('a2', thing2);
+    setChartLabels(cLabels);
+    // const [...cValues] = incomeCards.map((card) => card.income);
+    // const thing3 = cValues.valueOf;
+    // setChartValues(thing3);
+  }, []);
+
+  console.warn('a3', chartLabels);
+  // console.warn('a4', chartValues);
   return (
     <div className="income-view-container">
-      <div>
-        {/* <IncomeChart uid={uid} obj={chartData} /> */}
-        <IncomeChart uid={uid} />
+      <div style={{ width: '20rem' }}>
+        <Doughnut
+          // datasetIdKey="id"
+          data={{
+            labels: ['Jun', 'Jul', 'Aug'],
+            // labels: chartLabels,
+            datasets: [
+              {
+                id: 1,
+                label: '',
+                data: [5, 6, 7],
+              },
+            ],
+          }}
+        />
       </div>
       <hr />
       <div>
